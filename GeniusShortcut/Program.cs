7063319190c1938198d42a74be8d3da73a67e0ec
Ben <wildfirex00@gmail.com>
2020-05-01 02:09:21 -0400

    Added first working code

---
+++ GeniusShortcut/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GeniusShortcut
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        [STAThread]
+        static void Main()
+        {
+            //Make sure there's only one instance of the application running
+            bool result;
+            var mutex = new System.Threading.Mutex(true, "UniqueAppId", out result);
+
+            if (!result)
+            {
+                MessageBox.Show("Another instance of the application is already running", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Environment.Exit(1);
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+
+            GC.KeepAlive(mutex);
+        }
+    }
+}
